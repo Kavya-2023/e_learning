940ca02fd016c4d3fcc9be0e157976d49a5e96b2
kavya varanasi <varanasikavya0@gmail.com>
2024-04-23 17:17:04 +0530

    second

--- server/index.js
+++ server/index.js
@@ -21,15 +21,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  // You can add additional headers here if needed
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
 
 
-const allowedOrigins = ['http://localhost:3000', 'https://www.nanoquesttech.in'];
+const allowedOrigins = ['http://localhost:3000', 'https://www.nanoquesttech.in','https://e-learning-1-jycy.onrender.com'];
 app.use(cors({
   origin: function(origin, callback) {
 
